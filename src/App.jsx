75475e88c218d1690f1c69bad0bb48b4a14134c9
SavitarN <nischalzoc999@gmail.com>
2025-04-04 17:17:32 +0545

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 import LoadingSree from "./components/LoadingSree";
 import "./index.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [isLoaded, setIsLoading] = useState(false);
 
   return (
-    <>{!isLoaded && <LoadingSree onComplete={() => setIsLoading(true)} />}</>
+    <>
+      {!isLoaded && <LoadingSree onComplete={() => setIsLoading(true)} />}
+
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        <Navbar />
+      </div>
+    </>
   );
 }
 
--- src/components/LoadingSree.jsx
+++ src/components/LoadingSree.jsx
@@ -9,7 +9,7 @@ const LoadingSree = ({ onComplete }) => {
     const interval = setInterval(() => {
       setText(fullText.substring(0, index));
       index++;
-      console.log(interval);
+
       if (index > fullText.length) {
         clearInterval(interval);
 
@@ -22,10 +22,10 @@ const LoadingSree = ({ onComplete }) => {
   }, [onComplete]);
   return (
     <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
-      <div className="mb-4 text-4xl font-mono font-bold">
+      <div className="mb-4 text-4xl font-mono font-bold ">
         {text}
         <span className="animate-blink ml-1">|</span>
-        <div className="w-[250px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
+        <div className="w-[253px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
           <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
         </div>
       </div>
