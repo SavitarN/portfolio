e986fe125694482a3d3b5bd6aa1ea4bbf9aecef5
SavitarN <nischalzoc999@gmail.com>
2025-04-07 21:44:55 +0545

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import LoadingSree from "./components/LoadingSree";
 import "./index.css";
 import Navbar from "./components/Navbar";
+import MobileMenu from "./components/MobileMenu";
 
 function App() {
   const [isLoaded, setIsLoading] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
@@ -15,7 +17,8 @@ function App() {
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
-        <Navbar />
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Navbar = () => {
+const Navbar = ({ menuOpen, setMenuOpen }) => {
+  useEffect(() => {
+    document.body.style.overflow = menuOpen ? "hidden" : "";
+  }, [menuOpen]);
   return (
     <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
       <div className="max-w-5xl mx-auto px-4 ">
-        <div className="flex justify-between items-center h-16 ">
+        <div className="flex justify-between items-center h-16  ">
           <a href="#home" className="font-mono text-xl font-bold text-white">
             Nischal.Joshi
           </a>
           {/* mobile menu */}
-          <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden"></div>
+          <div
+            className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
+            onClick={() => setMenuOpen((prevValue) => !prevValue)}
+          >
+            &#9776
+          </div>
 
           {/* desktop menu */}
-          <div className="hidden md:flex items-center space-x-8">
+          <div className="hidden md:flex items-center space-x-8 ">
             <a
               href="#home"
               className="text-gray-300 hover:text-white transition-colors"
