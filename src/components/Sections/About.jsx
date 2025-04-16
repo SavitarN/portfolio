1698b2d365bc59ccfc89b20cabdd3742d42e3cde
SavitarN <nischalzoc999@gmail.com>
2025-04-16 17:44:14 +0545

    about section

--- src/components/Sections/About.jsx
+++ src/components/Sections/About.jsx
@@ -5,18 +5,20 @@ const About = () => {
     "Javascript (Es5+)",
     "React",
     "TailwindCSS",
+    "Problem-solving",
     "HTML",
     "CSS",
     "MySql",
     "PHP",
+    "Design",
   ];
 
   return (
     <section
       id="about "
-      className="min-h-screen border border-green-500 flex items-center justify-center py-20"
+      className="min-h-screenflex items-center justify-center py-20"
     >
-      <div className="max-w-3xl mx-auto px-4 border border-red-500">
+      <div className="max-w-3xl mx-auto px-4 ">
         <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-purple-500 to-cyan-400 bg-clip-text text-transparent text-center">
           About Me
         </h2>
@@ -29,20 +31,40 @@ const About = () => {
             building — let’s make something great together.
           </p>
 
-          <div className="grid grid-cols-1 md-grid-cols-2 gap-6 ">
-            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+          <div className="grid grid-cols-1  gap-6">
+            <div className="rounded-xl p-6 hover:-translate-y-1  ">
               <h3 className="text-xl font-bold mb-4 ">Skills</h3>
-              <div className="flex flex-wrap gap-2 ">
+              <div className="flex flex-wrap gap-2 justify-center ">
                 {Skills.map((tech, key) => (
                   <span
                     key={key}
-                    className="bg-purple-500/10 text-white-500 py-1 px-3 rounded-full text-sm hover:bg-purple/20 hover:shadow-[0_2px_8px_rgba[59,130,2246,0.2]] transition"
+                    className="bg-purple-500/10 text-white-500 py-1 px-3 rounded-full text-sm hover:bg-purple-600 hover:shadow-[0_2px_8px_rgba[59,130,2246,0.2]] transition"
                   >
                     {tech}
                   </span>
                 ))}
               </div>
             </div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6  mt-8 border border-red-500">
+          <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+            <h3 className="text-xl font-bold mb-4">📖 Education</h3>
+            <ul className="list-disc list-inside text-gray-300 space-y-2">
+              <li>
+                <strong>Bachelor's In Information Management</strong> Tribhuvan
+                University (2019-2025)
+              </li>
+              <li>
+                Relavant Coursework: Programming, Web Development, DSA, Software
+                Engineering, Management
+              </li>
+            </ul>
+          </div>
+
+          <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+            <h3 className="text-xl font-bold mb-4">💼 Work Experience</h3>
           </div>
         </div>
       </div>
