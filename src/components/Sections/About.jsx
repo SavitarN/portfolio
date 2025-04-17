79feea343f296efd288913ebda6a714f89162473
SavitarN <nischalzoc999@gmail.com>
2025-04-17 14:01:40 +0545

    projects section

--- src/components/Sections/About.jsx
+++ src/components/Sections/About.jsx
@@ -38,7 +38,7 @@ const About = () => {
                 {Skills.map((tech, key) => (
                   <span
                     key={key}
-                    className="bg-purple-500/10 text-white-500 py-1 px-3 rounded-full text-sm hover:bg-purple-600 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition"
+                    className="bg-purple-500/10 text-white-500 py-1 px-3 rounded-full text-sm hover:bg-purple-600 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition-all"
                   >
                     {tech}
                   </span>
--- src/components/Sections/Projects.jsx
+++ src/components/Sections/Projects.jsx
@@ -13,7 +13,7 @@ const Projects = () => {
         </h2>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition all">
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition-all">
             <h3 className="text-xl font-bold mb-2 ">
               Banking Transaction Demo
             </h3>
@@ -21,7 +21,7 @@ const Projects = () => {
               Banking transaction web app that enables user account login ,
               transfer and borrow money, sort transaction
             </p>
-            <div>
+            <div className="flex flex-wrap gap-2 mb-4">
               <span className="bg-purple-500/10 text-white-500 py-1 px-3 rounded-full text-sm hover:bg-purple-600 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition all">
                 Javascript
               </span>
@@ -43,7 +43,7 @@ const Projects = () => {
             </div>
           </div>
 
-          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition all">
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition-all">
             <h3 className="text-xl font-bold mb-2 ">
               Lab Equipment Management System
             </h3>
@@ -51,7 +51,7 @@ const Projects = () => {
               Banking transaction web app that enables user account login ,
               transfer and borrow money, sort transaction
             </p>
-            <div>
+            <div className="flex flex-wrap gap-2 mb-4">
               <span className="bg-purple-500/10 text-white-500 py-1 px-3 rounded-full text-sm hover:bg-purple-600 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition all">
                 Javascript
               </span>
@@ -73,7 +73,7 @@ const Projects = () => {
             </div>
           </div>
 
-          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition all">
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition-all ">
             <h3 className="text-xl font-bold mb-2 ">
               Lab Equipment Management System
             </h3>
@@ -81,7 +81,7 @@ const Projects = () => {
               Banking transaction web app that enables user account login ,
               transfer and borrow money, sort transaction
             </p>
-            <div>
+            <div className="flex flex-wrap gap-2 mb-4">
               <span className="bg-purple-500/10 text-white-500 py-1 px-3 rounded-full text-sm hover:bg-purple-600 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition all">
                 Javascript
               </span>
@@ -103,7 +103,7 @@ const Projects = () => {
             </div>
           </div>
 
-          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition all">
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition-all ">
             <h3 className="text-xl font-bold mb-2 ">
               Lab Equipment Management System
             </h3>
@@ -111,7 +111,7 @@ const Projects = () => {
               Banking transaction web app that enables user account login ,
               transfer and borrow money, sort transaction
             </p>
-            <div>
+            <div className="flex flex-wrap gap-2 mb-4">
               <span className="bg-purple-500/10 text-white-500 py-1 px-3 rounded-full text-sm hover:bg-purple-600 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition all">
                 Javascript
               </span>
@@ -133,7 +133,7 @@ const Projects = () => {
             </div>
           </div>
 
-          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition all">
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition-all ">
             <h3 className="text-xl font-bold mb-2 ">
               Lab Equipment Management System
             </h3>
@@ -141,7 +141,7 @@ const Projects = () => {
               Banking transaction web app that enables user account login ,
               transfer and borrow money, sort transaction
             </p>
-            <div>
+            <div className="flex flex-wrap gap-2 mb-4">
               <span className="bg-purple-500/10 text-white-500 py-1 px-3 rounded-full text-sm hover:bg-purple-600 hover:shadow-[0_2px_8px_rgba[59,130,246,0.1]] transition all">
                 Javascript
               </span>
